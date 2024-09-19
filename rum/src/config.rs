6924a95f8d3f93d950e2f6b1f9fd94c5c345a4f9
Lev Kokotov <lev.kokotov@gmail.com>
2024-09-18 23:30:28 -0700

    tty

--- rum/src/config.rs
+++ rum/src/config.rs
@@ -1,6 +1,7 @@
 use aes::Aes128;
 use aes_gcm_siv::{AesGcmSiv, Key};
 use once_cell::sync::{Lazy, OnceCell};
+use std::io::IsTerminal;
 use time::Duration;
 
 static CONFIG: OnceCell<Config> = OnceCell::new();
@@ -8,6 +9,7 @@ static CONFIG: OnceCell<Config> = OnceCell::new();
 pub struct Config {
     pub aes_key: Key<AesGcmSiv<Aes128>>, // AES-128 key used for encryption.
     pub cookie_max_age: Duration,
+    pub tty: bool,
 }
 
 impl Default for Config {
@@ -15,6 +17,7 @@ impl Default for Config {
         Self {
             aes_key: Key::<AesGcmSiv<Aes128>>::default(),
             cookie_max_age: Duration::days(30),
+            tty: std::io::stderr().is_terminal(),
         }
     }
 }
