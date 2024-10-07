f2f832a265d361ace75248062dd98664a54974c0
Lev Kokotov <lev.kokotov@gmail.com>
2024-10-07 00:38:53 -0700

    static

--- examples/static-files/src/main.rs
+++ examples/static-files/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rum::controller::StaticFiles;
+use rum::http::Server;
+use rum::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    Logger::init();
+
+    Server::new(vec![StaticFiles::serve("static")?])
+        .launch("0.0.0.0:8000")
+        .await?;
+
+    Ok(())
 }
