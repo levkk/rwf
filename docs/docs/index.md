bc99a96c2d990271d7527d68511a64bc9718e838
Lev Kokotov <lev.kokotov@gmail.com>
2024-10-16 09:10:07 -0700

    mkdocs

---
+++ docs/docs/index.md
@@ -0,0 +1,3 @@
+# Introduction
+
+[Rwf](https://github.com/levkk/rwf) is a framework for building web applications in the Rust programming language.
--- examples/quick-start/src/main.rs
+++ examples/quick-start/src/main.rs
@@ -1,22 +1,21 @@
-use rwf::http::Server;
+use rwf::http::{self, Server};
 use rwf::prelude::*;
 
 #[derive(Default)]
-struct IndexController;
+struct Index;
 
 #[async_trait]
-impl Controller for IndexController {
-    async fn handle(&self, _request: &Request) -> Result<Response, Error> {
-        Ok(Response::new().html("<h1>Hey Rum!</h1>"))
+impl Controller for Index {
+    async fn handle(&self, request: &Request) -> Result<Response, Error> {
+        Ok(Response::new().html("<h1>My first Rwf app!</h1>"))
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), http::Error> {
     Logger::init();
 
-    Server::new(vec![IndexController::default().route("/")])
+    Server::new(vec![route!("/" => Index)])
         .launch("0.0.0.0:8000")
         .await
-        .expect("error shutting down server");
 }
