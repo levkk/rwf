ef6aefb9e24517b7cb69bb70cd2c1b18461e7a81
Lev Kokotov <lev.kokotov@gmail.com>
2024-10-21 15:43:54 -0700

    delete unused

--- examples/django-asgi/debug_wsgi/wsgi.py
+++
@@ -1,5 +0,0 @@
-def application(env, start_response):
-    print(env)
-    body = [b'Hello']
-    start_response('200 OK', [('Content-Type', 'text/plain'), ('Content-Length', str(len(body)))])
-    return body
