ea4cbd6aaeb198e91481a3dac31e57c29f921364
Lev Kokotov <lev.kokotov@gmail.com>
2024-10-25 23:48:13 -0700

    Use Rayon instead of spawn_blocking for WSGI

--- rwf/src/controller/wsgi.rs
+++ rwf/src/controller/wsgi.rs
@@ -3,11 +3,14 @@ use crate::http::{wsgi::WsgiRequest, Request, Response};
 
 use async_trait::async_trait;
 use pyo3::prelude::*;
+use rayon::{ThreadPool, ThreadPoolBuilder};
+use tokio::sync::oneshot::channel;
 use tokio::time::{timeout, Duration};
 
 pub struct WsgiController {
     path: &'static str,
     timeout: Duration,
+    pool: ThreadPool,
 }
 
 impl WsgiController {
@@ -15,11 +18,20 @@ impl WsgiController {
         WsgiController {
             path,
             timeout: Duration::from_secs(60),
+            pool: ThreadPoolBuilder::new().num_threads(2).build().unwrap(),
         }
     }
 
     pub fn timeout(mut self, timeout: Duration) -> Self {
         self.timeout = timeout;
+        self
+    }
+
+    pub fn max_threads(mut self, threads: usize) -> Self {
+        self.pool = ThreadPoolBuilder::new()
+            .num_threads(threads)
+            .build()
+            .unwrap();
         self
     }
 }
@@ -29,25 +41,23 @@ impl Controller for WsgiController {
     async fn handle(&self, request: &Request) -> Result<Response, Error> {
         let request = WsgiRequest::from_request(request)?;
         let path = self.path;
+        let (tx, rx) = channel();
+
+        self.pool.spawn(move || {
+            let response = Python::with_gil(|py| {
+                // Import is cached.
+                let module = PyModule::import_bound(py, path).unwrap();
+                let application: Py<PyAny> = module.getattr("application").unwrap().into();
+                request.send(&application)
+            })
+            .unwrap();
+
+            tx.send(response).unwrap();
+        });
 
         // TODO: spawn blocking tasks cannot be aborted.
         // This only aborts waiting for the result to be returned.
-        match timeout(
-            self.timeout,
-            tokio::task::spawn_blocking(move || {
-                let response = Python::with_gil(|py| {
-                    // Import is cached.
-                    let module = PyModule::import_bound(py, path).unwrap();
-                    let application: Py<PyAny> = module.getattr("application").unwrap().into();
-                    request.send(&application)
-                })
-                .unwrap();
-
-                response
-            }),
-        )
-        .await?
-        {
+        match timeout(self.timeout, rx).await? {
             Ok(response) => Ok(response.to_response()?),
             Err(e) => Ok(Response::internal_error(e)),
         }
