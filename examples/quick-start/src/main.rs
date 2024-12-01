04e7e4a83ca9211fc22abdf6b85c8e551604ba93
Lev Kokotov <lev.kokotov@gmail.com>
2024-12-01 11:14:11 -0800

    add more examples to quick-start

--- examples/quick-start/src/main.rs
+++ examples/quick-start/src/main.rs
@@ -1,3 +1,6 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+
 use rwf::http::{self, Server};
 use rwf::prelude::*;
 
@@ -7,19 +10,54 @@ async fn index() -> Response {
     Response::new().html("<h1>My first Rwf app!</h1>")
 }
 
-/// More complex controller.
-#[derive(Default)]
-struct CurrentTime;
+/// Controller handler processing data from the request.
+#[controller]
+async fn with_request(request: &Request) -> Response {
+    let name = request
+        .query()
+        .get::<String>("name")
+        .unwrap_or(String::from(""));
+
+    let body = format!("<h2>Name: {name}</h2>");
+
+    Response::new().html(body)
+}
+
+/// Controller handler that could return an error.
+#[controller]
+async fn with_error(request: &Request) -> Result<Response, Error> {
+    let name = request.query().get_required::<String>("name")?;
+    let body = format!("<h2>Name: {name}</h2>");
+
+    Ok(Response::new().html(body))
+}
+
+/// More complex controller with internal state.
+struct RequestCount {
+    requests: Arc<AtomicUsize>,
+}
+
+// `route!` macro uses `Self::default()` to instantiate your controllers.
+impl Default for RequestCount {
+    fn default() -> Self {
+        Self {
+            requests: Arc::new(AtomicUsize::new(0)),
+        }
+    }
+}
 
 #[async_trait]
-impl Controller for CurrentTime {
-    /// This function responds to all incoming HTTP requests.
+impl Controller for RequestCount {
+    /// This function responds to incoming HTTP requests to this controller.
     async fn handle(&self, _req: &Request) -> Result<Response, Error> {
         let time = OffsetDateTime::now_utc();
+        let requests = self.requests.fetch_add(1, Ordering::Relaxed) + 1;
 
         // This creates an HTTP "200 OK" response,
         // with "Content-Type: text/plain" header.
-        let response = Response::new().text(format!("The current time is: {:?}", time));
+        let response = Response::new().html(format!(
+            "The current time is: {time:?} <br> Requests received: {requests}"
+        ));
 
         Ok(response)
     }
@@ -29,7 +67,12 @@ impl Controller for CurrentTime {
 async fn main() -> Result<(), http::Error> {
     Logger::init();
 
-    Server::new(vec![route!("/" => index), route!("/time" => CurrentTime)])
-        .launch()
-        .await
+    Server::new(vec![
+        route!("/" => index),
+        route!("/time" => RequestCount),
+        route!("/with-request" => with_request),
+        route!("/with-error" => with_error),
+    ])
+    .launch()
+    .await
 }
