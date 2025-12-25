f208e783ecd0597db89b7a3d08c552d00a38638a
builder <builder@zeus>
2025-12-25 17:52:13 +0100

    Intoduce DocString View

--- rwf/src/model/view.rs
+++ rwf/src/model/view.rs
@@ -7,6 +7,7 @@ use super::picked::Picked;
 use super::select::Select;
 use super::{Model, Query};
 
+/// Implements a joined View with Picked Columns
 #[derive(Debug, Clone)]
 pub struct View<T>
 where
@@ -15,6 +16,7 @@ where
     pivot: Picked<T>,
 }
 
+/// Default way to create a new one, is from a Picked one
 impl<T> From<Picked<T>> for View<T>
 where
     T: Model,
@@ -24,6 +26,7 @@ where
     }
 }
 
+/// For the sake of simplicity, a wrapper from a qualified Query
 impl<T> TryFrom<Query<T>> for View<T>
 where
     T: Model,
@@ -40,6 +43,7 @@ where
     }
 }
 
+/// Convert into a Query
 impl<T> Into<Query<T>> for View<T>
 where
     T: Model,
@@ -53,11 +57,13 @@ impl<T> View<T>
 where
     T: Model,
 {
+    /// Create a new View from a Select Query
     pub fn new(select: Select<T>) -> Self {
         Self {
             pivot: Picked::from(select),
         }
     }
+    /// Add a Column to tge view with Aggregation and alias
     pub fn add_aggregated_column(
         self,
         column: impl ToColumn,
@@ -70,16 +76,21 @@ where
         let piv = self.pivot.add_column(column, agg, alias);
         Self::from(piv)
     }
+    /// Add a Column without Aggregation and alias
     pub fn add_view_column(self, column: impl ToColumn, alias: Option<impl ToString>) -> Self {
         self.add_aggregated_column(column, "", alias)
     }
+    /// Join with another view including all Picked Columns
     pub fn join<U: Association<T>>(mut self, other: View<U>) -> Self {
         self.pivot.select.joins = self.pivot.select.joins.add(U::construct_join());
         Self::from(self.pivot.merge(other.pivot.columns()))
     }
+    /// Workaround as FromRow is only implementable for dedicated Structs
     pub fn from_row(&self, row: tokio_postgres::Row) -> Result<Self, Error> {
         self.pivot.from_row(row).map(|pick| Self::from(pick))
     }
+    /// For later Usage. Intended is a Macro which creates a Struct related to the Joined Query
+    /// implementing the Model Trait
     pub fn create_view(&self, name: impl ToString) -> String {
         format!(
             r#"CREATE VIEW "{}" AS ({})"#,
@@ -87,6 +98,7 @@ where
             self
         )
     }
+    /// Use all columns including the primary key from the main Table
     pub fn use_all_pivot() -> Self {
         Self::try_from(
             T::all()
@@ -95,11 +107,13 @@ where
         )
         .unwrap()
     }
+    /// Use all columns excluding the primary Key. used for the joined Table
     pub fn use_all() -> Self {
         Self::try_from(T::all().select_columns(T::column_names())).unwrap()
     }
 }
 
+/// Deref ToSql to Picked
 impl<T> ToSql for View<T>
 where
     T: Model,
@@ -109,6 +123,7 @@ where
     }
 }
 
+/// Deref Display to Picked
 impl<T> std::fmt::Display for View<T>
 where
     T: Model,
